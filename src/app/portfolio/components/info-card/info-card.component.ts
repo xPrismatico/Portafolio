0bd5a02c08fda41040f1f2c42a937541ea14b19c
xPrismatico <srfuentesavila@gmail.com>
2024-12-08 22:43:26 -0300

    feat: add info cards, projects and navbar

---
+++ src/app/portfolio/components/info-card/info-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'portfolio-info-card',
+  imports: [],
+  templateUrl: './info-card.component.html',
+  styleUrl: './info-card.component.css'
+})
+export class InfoCardComponent {
+
+}
--- src/app/portfolio/pages/portfolio-home/portfolio-home.component.ts
+++ src/app/portfolio/pages/portfolio-home/portfolio-home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
 import { AvatarComponent } from '../../components/avatar/avatar.component';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { InfoCardComponent } from '../../components/info-card/info-card.component';
 
 @Component({
   selector: 'portfolio-page-home',
-  imports: [CardComponent, AvatarComponent, NavbarComponent],
+  imports: [CardComponent, AvatarComponent, NavbarComponent, InfoCardComponent],
   templateUrl: './portfolio-home.component.html',
   styleUrl: './portfolio-home.component.css'
 })
