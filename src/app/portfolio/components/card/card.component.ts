82a1cfc4018ef5e6e3935c2f46b5270cce307c40
xPrismatico <srfuentesavila@gmail.com>
2025-03-17 00:53:09 -0300

    feat: add interactions, design, component order, movile navbar fixed

--- src/app/portfolio/components/card/card.component.ts
+++ src/app/portfolio/components/card/card.component.ts
@@ -1,11 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'portfolio-card',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
 export class CardComponent {
 
+
+  // Estado para controlar qué sinopsis está desplegada
+  public expandedSinopsis: { [key: string]: boolean } = {};
+
+
+  // Método para alternar el estado de la sinopsis
+  public toggleSinopsis(id: string): void {
+    this.expandedSinopsis[id] = !this.expandedSinopsis[id];
+  }
 }
--- src/app/portfolio/components/navbar/navbar.component.ts
+++ src/app/portfolio/components/navbar/navbar.component.ts
@@ -7,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  isMenuOpen = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+    const navbar = document.getElementById('navbar-sticky');
+    if (navbar) {
+      if (this.isMenuOpen) {
+        navbar.classList.remove('hidden');
+      } else {
+        navbar.classList.add('hidden');
+      }
+    }
+  }
 }
